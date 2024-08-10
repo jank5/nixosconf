e2c6990f02fdd4c014a8e86921a984b4fecc25f9
jank5 <klevzhic5jank@gmail.com>
2024-08-11 01:37:21 +0300

    setting zsh modules

--- home-manager/modules/zsh.nix
+++ home-manager/modules/zsh.nix
@@ -10,25 +10,26 @@
       let
         flakeDir = "~/nix";
       in {
-      	rb = "sudo nixos-rebuild switch --flake ${flakeDir}";
+      	rb = "doas nixos-rebuild switch --flake ${flakeDir}";
       	upd = "nix flake update ${flakeDir}";
-      	upg = "sudo nixos-rebuild switch --upgrade --flake ${flakeDir}";
-
+      	upg = "doas nixos-rebuild switch --upgrade --flake ${flakeDir}";
       	hms = "home-manager switch --flake ${flakeDir}";
 
       	conf = "nvim ${flakeDir}/nixos/configuration.nix";
-      	pkgs = "nvim ${flakeDir}/nixos/packages.nix";
 
-	c = "clear";
-      	ll = "ls -l";
+      	grep = "grep --color=auto"
+      	c = "clear";
+      	ls = "exa";
+      	l = "exa -l";
+      	la = "exa -la";
       	v = "nvim";
       	dv = "doas nvim";
       	ff = "fastfetch";
-	".." = "cd ..";
-	"..." = "cd ../..";
-	".3" = "cd ../../..";
-	".4" = "cd ../../../..";
-	".5" = "cd ../../../../..";
+	      ".." = "cd ..";
+	      "..." = "cd ../..";
+	      ".3" = "cd ../../..";
+	      ".4" = "cd ../../../..";
+	      ".5" = "cd ../../../../..";
     };
 
     oh-my-zsh = {
