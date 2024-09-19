03de1ef4e7ffe3ce02600a33ac4676d9b74d2d8f
jank5 <klevzhic5jank@gmail.com>
2024-09-19 15:48:22 +0300

    upd and nvidia drivers beta

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -60,7 +60,7 @@
     open = false;
 
     nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.beta;
   };
 
 # GNOME(Also if i want to change DE to Gnome, just uncomment this line)(And need uncomment package extensions)
