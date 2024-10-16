849724b6ac8bb70848b7752615c4667bdf4d456b
jank5 <klevzhic5jank@gmail.com>
2024-10-16 13:09:45 +0300

    add alias

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -1,8 +1,23 @@
 {
   programs.git = {
-	enable = true;
-	userName = "jank5";
-	userEmail = "klevzhic5jank@gmail.com";
+		enable = true;
+		userName = "jank5";
+		userEmail = "klevzhic5jank@gmail.com";
  };
+  aliases = {
+ 		a = "add";
+ 		c = "commit";
+ 		ca = "commit -a";
+ 		f = "fetch";
+ 		cl = "clone";
+ 		d = "diff";
+ 		ps = "push";
+ 		st = "status";
+ 		lol = "log --graph --decorate --pretty=oneline --abbrev-commit";
+		lola = "log --graph --decorate --pretty=oneline --abbrev-commit --all";
+ 	};
+ 	safe = {
+ 	directory = "*"
+ 	};
 }
 
