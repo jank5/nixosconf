74ede258b2458e743039673e5079dacae3429f01
jank5 <klevzhic5jank@gmail.com>
2024-11-19 18:15:34 +0300

    add safe directory path to .git

--- home-manager/modules/git.nix
+++ home-manager/modules/git.nix
@@ -5,7 +5,7 @@
 		userEmail = "klevzhic5jank@gmail.com";
 		extraConfig = {
       init.defaultBranch = "main";
-		  safe.directory = "/home/zemo/nixosconf/";
+		  safe.directory = "/home/zemo/nixosconf/.git";
 		};
 		includes = [
 			{ path = "/home/zemo/nixosconf/home-manager/modules/gitalias.txt"; }
